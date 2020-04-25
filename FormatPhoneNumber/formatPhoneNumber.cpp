87e66031db031decd848d54fe2825fd14c3f3a42
EswarAleti <eswaraleti143@gmail.com>
2020-04-25 08:00:10 +0530

    string concatenation done using + operator

--- FormatPhoneNumber/formatPhoneNumber.cpp
+++ FormatPhoneNumber/formatPhoneNumber.cpp
@@ -1,48 +1,48 @@
-#include<iostream>
-#include<string>
-using namespace std;
+	#include<iostream>
+	#include<string>
+	using namespace std;
 
-int main()
-{
-	string phoneNo;
-	cout<<"Please enter a phone number: ";
-	//User input
-	getline(cin,phoneNo);
-
-	string digits="";
-	//Store digits of phoneNo into digits
-	for(int i=0;i<phoneNo.length();i++)
+	int main()
 	{
-		char digit=phoneNo[i];
-		//If it is digit then store
-		if(isdigit(digit))
+		string phoneNo;
+		cout<<"Please enter a phone number: ";
+		//User input
+		getline(cin,phoneNo);
+
+		string digits="";
+		//Store digits of phoneNo into digits
+		for(int i=0;i<phoneNo.length();i++)
 		{
-			digits.push_back(digit);
+			char digit=phoneNo[i];
+			//If it is digit then store
+			if(isdigit(digit))
+			{
+				digits.push_back(digit);
+			}
 		}
-	}
-	//If number of digits less than 10
-	if(digits.length()<10)
-	{
-		cout<<"The phone number must have at least 10 digits"<<endl;
+		//If number of digits less than 10
+		if(digits.length()<10)
+		{
+			cout<<"The phone number must have at least 10 digits"<<endl;
+			return 0;
+		}
+		string formattedPhoneNo="(";
+		for(int i=0;i<10;i++)
+		{
+			//Add digit to the end of formattedPhoneNo
+			formattedPhoneNo+=digits[i];
+			//After appending digit at index 2 apend ") "
+			if(i==2)
+			{
+				formattedPhoneNo+=") ";
+			}
+			//After appending digit at index 5 apend "-"
+			if(i==5)
+			{
+				formattedPhoneNo+="-";
+			}
+		}
+		//Print output
+		cout<<"The properly formatted number is "<<formattedPhoneNo<<endl;
 		return 0;
 	}
-	string formattedPhoneNo="(";
-	for(int i=0;i<10;i++)
-	{
-		//Add digit to the end of formattedPhoneNo
-		formattedPhoneNo+=digits[i];
-		//After appending digit at index 2 apend ") "
-		if(i==2)
-		{
-			formattedPhoneNo+=") ";
-		}
-		//After appending digit at index 5 apend "-"
-		if(i==5)
-		{
-			formattedPhoneNo+="-";
-		}
-	}
-	//Print output
-	cout<<"The properly formatted number is "<<formattedPhoneNo<<endl;
-	return 0;
-}
