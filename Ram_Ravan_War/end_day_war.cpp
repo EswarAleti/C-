c28d03c85d9e66f22d1171b8487fb5391eddbbc4
EswarAleti <eswaraleti143@gmail.com>
2020-05-06 19:29:00 +0530

    Comments has been added

--- Ram_Ravan_War/end_day_war.cpp
+++ Ram_Ravan_War/end_day_war.cpp
@@ -4,15 +4,21 @@ using namespace std;
 int main()
 {
 	long long n,m;
+	//User input
 	cin>>n>>m;
+	//Start day from 1
 	long long i=1, max=n;
+	//Till Ram has no arrows
 	while(n>0)
 	{
-		cout<<"Ram: "<<n<<" Ravan: "<<i<<endl;
+		//Add at most m arrows
 		n=(n+m)<=max?(n+m):max;
+		//Destory i arrows of ram
 		n-=i;
+		//Increment day by 1
 		i++;
 	}
+	//Output
 	cout<<i-1<<endl;
 	return 0;
 }
